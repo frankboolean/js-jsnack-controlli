4f025a927326cdfd6fa7c54f37e3d41b524bbdcd
Francesco <fra.iozzia@gmail.com>
2020-03-20 18:12:29 +0100

    fine ultimo es

---
+++ numero/main.js
@@ -0,0 +1,16 @@
+// primo sec ecc
+
+var primo= parseInt(prompt("Mi dica un primo numero"));
+
+var secondo= parseInt(prompt("Mi dica un secondo numero"));
+
+var terzo= parseInt(prompt("Mi dica un terzo numero"));
+
+var quarto= parseInt(prompt("Mi dica un quarto numero"));
+
+var quinto= parseInt(prompt("Mi dica un quinto numero"));
+
+
+var somma= primo + secondo + terzo + quarto + quinto;
+
+document.getElementById('msg').innerHTML= "la somma dei seguenti numeri da risultato di " + somma;
